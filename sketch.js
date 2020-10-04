ec40e99c305e52a0af054697cbda3ba66eb8cd70
SHUBHAM KUMAR <39937550+shubhamkrshandilya@users.noreply.github.com>
2020-10-04 16:26:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,27 +3,24 @@ function setup() {
 }
 function draw(){
   background(0);
-  strokeWeight(4);
-  var sec = second();
-  var min = minute();
-  
-  var hrs = hour();
-  var mer = hrs<12?"AM":"PM";
-  sec = formatting(sec);
-  min = formatting(min);
-  hrs = formatting(hrs%12);
-
-  
+  var sec = radians(6*second());
+  var min = radians(6*minute());
+  var hrs = radians(30*(hour()%12));
+  //println(second());
   fill(255);
-  textSize(48);
-
-  textAlign(CENTER, CENTER);
-  text(hrs+":"+ min+":"+sec+" "+mer, width/2, height/2);
-}
-
-function formatting(num){
-  if(int(num)<10){
-    return "0"+num;
-  }
-  return num;
+  ellipse(width/2,height/2,400,400);
+  translate(width/2,height/2);
+  //rotate(-PI/2);
+  fill(0);
+  textSize(32);
+  text("12",-16,-170);
+  text("3",170,0);
+  text("6",0,190);
+  text("9",-190,0);
+  strokeWeight(4);
+  line(0,0,100*cos(hrs-PI/2),100*sin(hrs-PI/2));
+  strokeWeight(3);
+  line(0,0,120*cos(min-PI/2),120*sin(min-PI/2));
+  strokeWeight(2);
+  line(0,0,140*cos(sec-PI/2),140*sin(sec-PI/2));
 }
